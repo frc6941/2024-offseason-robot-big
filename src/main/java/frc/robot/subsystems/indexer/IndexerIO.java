9695992d4fb935e9b9023e6be9c75b4456f51050
hs150521 <hs150521@163.com>
2024-08-11 13:44:24 +0800

    fix: indexer PID

--- src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIO.java
@@ -1,9 +1,13 @@
 package frc.robot.subsystems.indexer;
 
-import edu.wpi.first.units.*;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Current;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Velocity;
 import org.littletonrobotics.junction.AutoLog;
 
 import static edu.wpi.first.units.Units.*;
+import static frc.robot.Constants.IndexerConstants.indexerGainsClass.*;
 
 public interface IndexerIO {
     void updateInputs(IndexerIOInputs inputs);
@@ -16,5 +20,11 @@ public interface IndexerIO {
         public Measure<Angle> indexPosition = Radians.zero();
         public double indexAppliedRPM = 0;
         public Measure<Current> indexSupplyCurrent = Amps.zero();
+        public double IndexerKP = INDEXER_KP.get();
+        public double IndexerKI = INDEXER_KI.get();
+        public double IndexerKD = INDEXER_KD.get();
+        public double IndexerKA = INDEXER_KA.get();
+        public double IndexerKV = INDEXER_KV.get();
+        public double IndexerKS = INDEXER_KS.get();
     }
 }
