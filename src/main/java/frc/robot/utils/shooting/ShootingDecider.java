4f5b26d528ad221944aa3e701b2347175bc8ae78
hs150521 <hs150521@163.com>
2024-08-16 23:36:20 +0800

    updatedd far shooting

--- src/main/java/frc/robot/utils/shooting/ShootingDecider.java
+++ src/main/java/frc/robot/utils/shooting/ShootingDecider.java
@@ -42,7 +42,9 @@ public class ShootingDecider implements Updatable {
         speakerParams.loadParameter(3.06, -5700, 63.728);// 20240808
         speakerParams.loadParameter(3.47, -5700, 67.333);// 20240808
         speakerParams.loadParameter(3.79, -5700, 70.635);//
-        speakerParams.loadParameter(3.9, -5700, 71.0312);// 20240808
+        speakerParams.loadParameter(3.9, -5700, 71.0312);
+        speakerParams.loadParameter(4.2, -5700, 74.0312);
+        speakerParams.loadParameter(4.5, -5700, 77.0312);// 20240808
         speakerParams.ready();
 
         highFerryParams.loadParameter(5.0, -2000.0, 10.0);
