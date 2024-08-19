e5e07e1103498eb302bebc800438aa482669837d
hs150521 <hs150521@163.com>
2024-08-19 18:28:51 +0800

    revert: 20240818 20:46

--- src/main/java/frc/robot/subsystems/limelight/Light.java
+++ src/main/java/frc/robot/subsystems/limelight/Light.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.limelight;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.swerve.Swerve;
+import frc.robot.utils.AllianceFlipUtil;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,19 +27,22 @@ public class Light extends SubsystemBase {
 
     public void lightOff() {
         light.set(true);
+        SmartDashboard.putBoolean("light", false);
     }
 
     public void lightOn() {
         light.set(false);
+        SmartDashboard.putBoolean("light", true);
     }
 
     @Override
     public void periodic() {
-        if ((this.getState() == STATE.ON ||
-                Math.abs(Limelight.getInstance().getSpeakerRelativePosition().getAngle().getDegrees() -
-                        Swerve.getInstance().getGyro().getYaw().getDegrees() -
-                        Swerve.getInstance().getGyro().getYaw().getDegrees()) < 40.0)
-                && this.getState() != STATE.OFF) {
+        if (this.getState() == STATE.ON || (
+                Math.abs(
+                        Math.abs(Limelight.getInstance().getSpeakerRelativePosition().getAngle().getDegrees()
+                                - Swerve.getInstance().getGyro().getYaw().getDegrees())
+                                - (AllianceFlipUtil.shouldFlip() ? 180 : 0)) < 20.0
+                        && this.getState() != STATE.OFF)) {
             Light.getInstance().lightOn();
         } else {
             Light.getInstance().lightOff();
