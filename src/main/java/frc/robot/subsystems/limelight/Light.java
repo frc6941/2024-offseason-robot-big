6dbf47b0a61a408eab122bddf238153e860bb749
hs150521 <hs150521@163.com>
2024-08-21 23:17:58 +0800

    fix:light

--- src/main/java/frc/robot/subsystems/limelight/Light.java
+++ src/main/java/frc/robot/subsystems/limelight/Light.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems.limelight;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.swerve.Swerve;
 import frc.robot.utils.AllianceFlipUtil;
 import lombok.Getter;
 import lombok.Setter;
+import org.frcteam6941.utils.AngleNormalization;
 
 public class Light extends SubsystemBase {
     private final DigitalOutput light = new DigitalOutput(0);
@@ -39,9 +43,16 @@ public class Light extends SubsystemBase {
     public void periodic() {
         if (this.getState() == STATE.ON || (
                 Math.abs(
-                        Math.abs(Limelight.getInstance().getSpeakerRelativePosition().getAngle().getDegrees()
-                                - Swerve.getInstance().getGyro().getYaw().getDegrees())
-                                - (AllianceFlipUtil.shouldFlip() ? 180 : 0)) < 20.0
+                        Swerve.getInstance().getGyro().getYaw().getDegrees() -
+                                AngleNormalization.placeInAppropriate0To360Scope(Swerve.getInstance().getGyro().getYaw().getDegrees(),
+                                AllianceFlipUtil.apply(
+                                        Constants.FieldConstants.Speaker.centerSpeakerOpening
+                                        .minus(new Translation3d(0.4, 0, 0)))
+                                .toTranslation2d()
+                                .minus(Swerve.getInstance().getLocalizer().getCoarseFieldPose(0).getTranslation())
+                                .rotateBy(Rotation2d.fromDegrees(180)).getAngle().getDegrees())) < 20.0
+                        && AllianceFlipUtil.apply(Swerve.getInstance().getLocalizer().getCoarseFieldPose(0)).getX()
+                            < Constants.FieldConstants.wingX
                         && this.getState() != STATE.OFF)) {
             Light.getInstance().lightOn();
         } else {
